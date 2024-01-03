53b7d481538214d71d9eee13411c66bdc958c70a
yusai <jusaira21@gmail.com>
2024-01-03 13:40:57 -0700

    Se imprimen las variables creadas

--- 00_challenge/bombon.cpp
+++ 00_challenge/bombon.cpp
@@ -16,14 +16,23 @@ const string lenguaje = "C++";
 
 int main() {
     // Tipos de variables:
-    int entero;
-    float decimal;
-    double doble;   // Decimales con mayor presicion
-    bool booleana;
-    char caracter;
-    string cadena;
+    int entero = 10;
+    float decimal = 3.14;
+    double doble = 3.14;   // Decimales con mayor presicion
+    bool booleana = true;
+    bool booleana2 = false;
+    char caracter = 'A';
+    string cadena = "flor de dragon";
 
     cout << "Hola, " << lenguaje << "!" << endl;
+
+    cout << entero << endl;
+    cout << decimal << endl;
+    cout << doble << endl;
+    cout << booleana << endl;
+    cout << booleana2 << endl;
+    cout << caracter << endl;
+    cout << cadena << endl;
 
     return 0;
 }
